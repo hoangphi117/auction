b4b42a2881461b1548dfe6f6b470151739b49562
hoangphi117 <nghphi1206@gmail.com>
2026-03-26 17:25:02 +0700

    refactor: remove unused wrapper functions from systemSetting model

--- src/models/systemSetting.model.js
+++ src/models/systemSetting.model.js
@@ -8,20 +8,10 @@ export function getSettings() {
     return db('system_settings').first();
 }
 
-export function getSetting(key) {
-    return db('system_settings')
-        .where({ key })
-        .first();
-}
 
 export function updateSetting(key, value) {
     return db('system_settings')
         .update({ value })
         .where({ key });
 }
-
-export function editNewProductLimitMinutes(minutes) {
-    return db('system_settings')
-        .update({ value: minutes })
-        .where({ key: 'new_product_limit_minutes' });
-}
+
